25f7073277a840a3d59063b6721865f28258d310
ezequielpsm <ezequielpacheco2@outlook.com>
2025-04-03 21:28:35 -0300

    Adiciona configurações do FireBase

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+import { ConfigModule } from '@nestjs/config';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { FirebaseModule } from './firebase/firebase.module';
+import { UsersModule } from './users/users.module';
+//import { RolesModule } from './roles/roles.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    FirebaseModule.forRoot(),
+    AuthModule,
+    UsersModule,
+    //RolesModule,
+  ],
   providers: [AppService],
 })
 export class AppModule {}
